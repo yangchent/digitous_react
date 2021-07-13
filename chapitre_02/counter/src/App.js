79838f795996e08bfd34244b4970a6c86142c7f5
yangchen <gyachungriom@gmail.com>
2021-07-13 15:52:37 +0200

    updates

--- chapitre_02/counter/src/App.js
+++ chapitre_02/counter/src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
-
+import Counter from "./Counter";
 
 class App extends React.Component {
-  constructor(){
-    super();
-  
-    this.state = {
-      count : 0,
-    };
-  }
   render() {
     return(
       <div>
         <h1>Counter</h1>
-        <h2>{this.state.count}</h2>
-        <button 
-        onClick={()=>  {
-          this.setState({
-            count: this.state.count - 1,
-            });
-          }}
-          style= {{backgroundColor:"green"}}>-</button>
-        <button 
-        onClick={()=>  {
-          this.setState({
-            count: this.state.count+ 1,
-            });
-          }}style= {{backgroundColor:"red"}}>+</button>
-
+        <Counter />
       </div>
     )
   }
--- chapitre_02/counter/src/counter.js
+++ chapitre_02/counter/src/counter.js
@@ -1,3 +1,31 @@
 import React from "react";
 
-class counter extends React.Component{}
+class Counter extends React.Component{
+    constructor(){
+        super();
+            this.state = {
+                count : 0,
+            };
+    }
+    count= ()=> {
+        this.setState({
+            count: this.state.count - 1,
+            });
+    }
+    render(){
+        return(
+            <div>
+                <h2>{this.state.count}</h2>
+                <button onclick ={this.count} style= {{backgroundColor:"green"}}>-</button>
+                <button 
+                    onClick={()=>  {
+                        this.setState({
+                    count: this.state.count+ 1,
+                    });
+                }}
+                style= {{backgroundColor:"red"}}>+</button>
+            </div>
+        )
+    }
+}
+export default Counter;
