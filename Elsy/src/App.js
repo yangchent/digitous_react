23ac5c59130c8bbe96d8415963c2e17ec8f1496c
yangchen <gyachungriom@gmail.com>
2021-08-10 14:37:31 +0200

    in progress chnaging to hooks

--- Elsy/src/App.js
+++ Elsy/src/App.js
@@ -1,10 +1,7 @@
-// chemin qui commence sans ./ -> une librairie
-import React from "react";
-// Global CSS
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "./styles/global.css";
-// Components
 import Box from "./components/Box";
 
 const tempMin = -20;
@@ -14,108 +11,195 @@ const heartMax = 180;
 const stepsMin = 0;
 const stepsMax = 50000;
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-        water : 0,
-        heart : 120,
-        temperature : -10,
-        steps : 3000
-    }
-    
-  }
-  onHeartChange=(e)=>{
-    this.setState((prevState) => {
-      return{
-        ...prevState,
-        heart : e.target.value,
-        water :this.calculateWater(prevState, e.target.value)
-      } 
-    });
-  }
-  onHeatChange=(e)=>{
-    this.setState((prevState) => {
-      return{
-        ...prevState,
-        temperature : e.target.value ,
-        water :this.calculateWater(prevState, e.target.value)
-    }
-  })
-  }
-  onDirectionChange=(e)=>{
-    this.setState((prevState) => {
-      return{
-        ...prevState,
-        steps : e.target.value ,
-        water :this.calculateWater(prevState, e.target.value)
-      }
-    })
-  }
-  calculateWater = (newState)=> {
-    let water1=0
-    let water2=0
-    let water3=0
-    if(this.state.temperature > 20){
-      water1 = .02*(this.state.temperature -20)
-    }
-   else if(this.state.heart >120){
-    water2 = 0.0008*(this.state.heart -120)
-    }
-    else if (this.state.steps >10000){
-      water3 = 0.00002*(this.state.steps-10000) 
-    }
-     let total = water1 + water2 + water3;
-      this.setState({water : (total + 1.5)} )
-        }
+const App= ()=> {
+  const [water, setWater] = useState(0);
+  const [heart, setHeart] = useState(120);
+  const [temperature, setTemperature] = useState(-10);
+  const [steps, setSteps] = useState(3000);
 
-	render() {
-		return (
-			<div className="container-fluid">
-				<div className="row">
-					{/* <p>Heart: {heartMin}</p>
-					<p>Temperature : {tempMin}</p>
-					<p>Steps : {stepsMin}</p> */}
-					<div className="row">
-						<Box
-							icon="local_drink"
-							color="#3A85FF"
-							value={this.state.water}
-							unit="L"   
-              onChange={this.calculateWater}         
-						/>
-						<Box
-							icon="directions_walk"
-							color="black"
-							value={this.state.steps}
-							unit="steps"
-              min= {stepsMin}
-              max= {stepsMax}
-              onChange={this.onDirectionChange}
-						/>
-						<Box
-							icon="favorite"
-							color="red"
-							value={this.state.heart}
-							unit="bpm"
-              min= {heartMin}
-              max= {heartMax}
-              onChange={this.onHeartChange}
-						/>
-						<Box
-							icon="wb_sunny"
-							color="yellow"
-							value={this.state.temperature}
-							unit="°C"
-              min = {tempMin}
-              max= {tempMax}
-              onChange={this.onHeatChange}
-						/>
-					</div>
-				</div>
-			</div>
-		);
-	}
-}
+const onHeartChange=(e)=>{
+  return setHeart(e.target.value),
+      calculateWater(e.target.value)
+    } 
+const onHeatChange=(e)=>{
+  return setTemperature(e.target.value ),
+      calculateWater(e.target.value)
+  }
+const onDirectionChange=(e)=>{
+  return setSteps(e.target.value),
+  calculateWater(e.target.value)
+    // (prevState) => {
+    // return{
+    //   ...prevState,
+    //   steps : e.target.value ,
+      // water :calculateWater(prevState, e.target.value)
+    }
+
+ const calculateWater = () => {
+  let water1=0
+  let water2=0
+  let water3=0
+  if(temperature > 20){
+    water1 = .02*(temperature -20)
+  }
+ else if(heart >120){
+  water2 = 0.0008*(heart -120)
+  }
+  else if (steps >10000){
+    water3 = 0.00002*(steps-10000) 
+  }
+   let total = water1 + water2 + water3;
+    setWater(total + 1.5)
+      }
+      return (
+        <div className="container-fluid">
+          <div className="row">
+            {/* <p>Heart: {heartMin}</p>
+            <p>Temperature : {tempMin}</p>
+            <p>Steps : {stepsMin}</p> */}
+            <div className="row">
+              <Box
+                icon="local_drink"
+                color="#3A85FF"
+                value={water}
+                unit="L"   
+                onChange={calculateWater}         
+              />
+              <Box
+                icon="directions_walk"
+                color="black"
+                value={steps}
+                unit="steps"
+                min= {stepsMin}
+                max= {stepsMax}
+                onChange={onDirectionChange}
+              />
+              <Box
+                icon="favorite"
+                color="red"
+                value={heart}
+                unit="bpm"
+                min= {heartMin}
+                max= {heartMax}
+                onChange={onHeartChange}
+              />
+              <Box
+                icon="wb_sunny"
+                color="yellow"
+                value={temperature}
+                unit="°C"
+                min = {tempMin}
+                max= {tempMax}
+                onChange={onHeatChange}
+              />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  
+// class App extends React.Component {
+  // constructor(){
+  //   super();
+  //   this.state = {
+  //       water : 0,
+  //       heart : 120,
+  //       temperature : -10,
+  //       steps : 3000
+  //   } 
+  // }
+  
+  // onHeartChange=(e)=>{
+  //   this.setState((prevState) => {
+  //     return{
+  //       ...prevState,
+  //       heart : e.target.value,
+  //       water :this.calculateWater(prevState, e.target.value)
+  //     } 
+  //   });
+  // }
+  // onHeatChange=(e)=>{
+  //   this.setState((prevState) => {
+  //     return{
+  //       ...prevState,
+  //       temperature : e.target.value ,
+  //       water :this.calculateWater(prevState, e.target.value)
+  //   }
+  // })
+  // }
+  // onDirectionChange=(e)=>{
+  //   this.setState((prevState) => {
+  //     return{
+  //       ...prevState,
+  //       steps : e.target.value ,
+  //       water :this.calculateWater(prevState, e.target.value)
+  //     }
+  //   })
+  // }
+  // calculateWater = (newState)=> {
+  //   let water1=0
+  //   let water2=0
+  //   let water3=0
+  //   if(this.state.temperature > 20){
+  //     water1 = .02*(this.state.temperature -20)
+  //   }
+  //  else if(this.state.heart >120){
+  //   water2 = 0.0008*(this.state.heart -120)
+  //   }
+  //   else if (this.state.steps >10000){
+  //     water3 = 0.00002*(this.state.steps-10000) 
+  //   }
+  //    let total = water1 + water2 + water3;
+  //     this.setState({water : (total + 1.5)} )
+  //       }
+
+	// render() {
+// 		return (
+// 			<div className="container-fluid">
+// 				<div className="row">
+// 					{/* <p>Heart: {heartMin}</p>
+// 					<p>Temperature : {tempMin}</p>
+// 					<p>Steps : {stepsMin}</p> */}
+// 					<div className="row">
+// 						<Box
+// 							icon="local_drink"
+// 							color="#3A85FF"
+// 							value={this.state.water}
+// 							unit="L"   
+//               onChange={this.calculateWater}         
+// 						/>
+// 						<Box
+// 							icon="directions_walk"
+// 							color="black"
+// 							value={this.state.steps}
+// 							unit="steps"
+//               min= {stepsMin}
+//               max= {stepsMax}
+//               onChange={this.onDirectionChange}
+// 						/>
+// 						<Box
+// 							icon="favorite"
+// 							color="red"
+// 							value={this.state.heart}
+// 							unit="bpm"
+//               min= {heartMin}
+//               max= {heartMax}
+//               onChange={this.onHeartChange}
+// 						/>
+// 						<Box
+// 							icon="wb_sunny"
+// 							color="yellow"
+// 							value={this.state.temperature}
+// 							unit="°C"
+//               min = {tempMin}
+//               max= {tempMax}
+//               onChange={this.onHeatChange}
+// 						/>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// }
 
 export default App;
--- Elsy/src/components/Box.js
+++ Elsy/src/components/Box.js
@@ -1,37 +1,63 @@
-import React from "react";
+import React, {props} from "react";
 
-class Box extends React.Component {
-	render() {
-		const addRange=()=> {
-		if(this.props.icon !== "local_drink"){ 
-		 return <input onChange={this.props.onChange} type="range" min={this.props.min} max={this.props.max} value= {this.props.value}></input>
+const Box =(props)=> {
+	const addRange=()=> {
+		if(props.icon !== "local_drink"){ 
+		 return <input onChange={props.onChange} type="range" min={props.min} max={props.max} value= {props.value}></input>
 		}}
-		// if(this.props.unit!= "L"){
-		// 	return <input type="range" />
-		// }
-
 		return (
 			<div className="box col-sm-3 col-6">
 				<span
 					className="material-icons"
 					style={{
 						color:
-							this.props.color === undefined
+							props.color === undefined
 								? "blue"
-								: this.props.color,
+								: props.color,
 						fontSize: 100,
 					}}
 				>
-					{this.props.icon}
+					{props.icon}
 				</span>
 				
 				<p>
-					{this.props.value} {this.props.unit}
+					{props.value} {props.unit}
 				</p>
 				{addRange()}
 			</div>
 		);
-	}
 }
+
+
+// class Box extends React.Component {
+// 	render() {
+// 		const addRange=()=> {
+// 		if(this.props.icon !== "local_drink"){ 
+// 		 return <input onChange={this.props.onChange} type="range" min={this.props.min} max={this.props.max} value= {this.props.value}></input>
+// 		}}
+
+// 		return (
+// 			<div className="box col-sm-3 col-6">
+// 				<span
+// 					className="material-icons"
+// 					style={{
+// 						color:
+// 							this.props.color === undefined
+// 								? "blue"
+// 								: this.props.color,
+// 						fontSize: 100,
+// 					}}
+// 				>
+// 					{this.props.icon}
+// 				</span>
+				
+// 				<p>
+// 					{this.props.value} {this.props.unit}
+// 				</p>
+// 				{addRange()}
+// 			</div>
+// 		);
+// 	}
+// }
 
 export default Box;
